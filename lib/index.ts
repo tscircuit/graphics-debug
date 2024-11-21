8c592484b55c0fdc27bf696f4eebd892a076920c
seveibar <seveibar@gmail.com>
2024-11-20 16:13:22 -0800

    wip

--- lib/index.ts
+++ lib/index.ts
@@ -1,17 +1,23 @@
 export type { Point, Line, Rect, Circle, GraphicsObject } from './types'
 export { getGraphicsObjectsFromLogString } from './getGraphicsObjectsFromLogString'
+export { getSvgFromGraphicsObject } from './getSvgFromGraphicsObject'
 
 export function getSvgFromLogString(logString: string): string {
-  // TODO: Implement
-  return ''
+  const objects = getGraphicsObjectsFromLogString(logString)
+  if (objects.length === 0) return ''
+  return getSvgFromGraphicsObject(objects[0])
 }
 
 export function getMarkdownFromLogString(logString: string): string {
-  // TODO: Implement
-  return ''
+  const svg = getSvgFromLogString(logString)
+  if (!svg) return ''
+  return `# Debug Graphics\n\n${svg}`
 }
 
 export function getSvgsFromLogString(logString: string): Array<{ title: string; svg: string }> {
-  // TODO: Implement
-  return []
+  const objects = getGraphicsObjectsFromLogString(logString)
+  return objects.map(obj => ({
+    title: obj.graphics.title || 'Untitled Graphic',
+    svg: getSvgFromGraphicsObject(obj)
+  }))
 }
