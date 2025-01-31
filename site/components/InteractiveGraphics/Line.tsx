60520f8da7023b9b0105ee40cc03a6fa3c98f7ba
Severin Ibarluzea <seve700@gmail.com>
2025-01-31 17:59:54 -0500

    add full width to svg (#22)

--- site/components/InteractiveGraphics/Line.tsx
+++ site/components/InteractiveGraphics/Line.tsx
@@ -63,6 +63,8 @@ export const Line = ({
         left: 0,
         right: 0,
         bottom: 0,
+        width: "100%",
+        height: "100%",
       }}
       onMouseMove={handleMouseMove}
       onMouseLeave={() => setIsHovered(false)}
