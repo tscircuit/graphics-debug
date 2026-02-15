ea9c3ad47d68038dda3c450ae21ccc8554f55b96
seveibar <seveibar@gmail.com>
2026-02-15 13:04:49 -0800

    add infiniteLine to fixture

--- examples/interactive1.fixture.tsx
+++ examples/interactive1.fixture.tsx
@@ -1,7 +1,21 @@
-import { InteractiveGraphics } from "site/components/InteractiveGraphics/InteractiveGraphics"
 import exampleGraphics from "site/assets/exampleGraphics.json"
+import { InteractiveGraphics } from "site/components/InteractiveGraphics/InteractiveGraphics"
 import type { GraphicsObject } from "../lib"
 
 export default () => {
-  return <InteractiveGraphics graphics={exampleGraphics as GraphicsObject} />
+  return (
+    <InteractiveGraphics
+      graphics={{
+        ...(exampleGraphics as GraphicsObject),
+        infiniteLines: [
+          {
+            origin: { x: 0, y: 0 },
+            directionVector: { x: 1, y: 0 },
+            strokeColor: "#666",
+            strokeDash: [4, 4],
+          },
+        ],
+      }}
+    />
+  )
 }
