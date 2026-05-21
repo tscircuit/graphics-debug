4e3ef24ef8117e3b18984d4892a07c27bc84847d
Landis Code <250052214+landiscode@users.noreply.github.com>
2026-05-21 00:39:51 -0700

    Filter arrows by active layer and step

--- site/components/InteractiveGraphics/hooks/useFilterArrows.ts
+++ site/components/InteractiveGraphics/hooks/useFilterArrows.ts
@@ -8,45 +8,67 @@ type LineCheck = (p1: Point, p2: Point) => boolean
 
 type PointCheck = (point: Point) => boolean
 
+type LayerAndStepCheck = (obj: { layer?: string; step?: number }) => boolean
+
 type UseFilterArrowsParams = {
   isPointOnScreen: PointCheck
   doesLineIntersectViewport: LineCheck
+  filterLayerAndStep: LayerAndStepCheck
+}
+
+export const isArrowVisible = (
+  arrow: Arrow,
+  {
+    isPointOnScreen,
+    doesLineIntersectViewport,
+    filterLayerAndStep,
+  }: UseFilterArrowsParams,
+) => {
+  if (!filterLayerAndStep(arrow)) {
+    return false
+  }
+
+  const geometry = getArrowGeometry(arrow)
+  const { shaftStart, shaftEnd, heads } = geometry
+
+  if (
+    isPointOnScreen(shaftStart) ||
+    isPointOnScreen(shaftEnd) ||
+    heads.some(
+      (head) =>
+        isPointOnScreen(head.tip) ||
+        isPointOnScreen(head.leftWing) ||
+        isPointOnScreen(head.rightWing) ||
+        isPointOnScreen(head.base),
+    )
+  ) {
+    return true
+  }
+
+  const segments: Array<[Point, Point]> = [
+    [shaftStart, shaftEnd],
+    ...heads.flatMap((head) => [
+      [head.base, head.leftWing] as [Point, Point],
+      [head.leftWing, head.tip] as [Point, Point],
+      [head.tip, head.rightWing] as [Point, Point],
+      [head.rightWing, head.base] as [Point, Point],
+    ]),
+  ]
+
+  return segments.some(([p1, p2]) => doesLineIntersectViewport(p1, p2))
 }
 
 export const useFilterArrows = ({
   isPointOnScreen,
   doesLineIntersectViewport,
+  filterLayerAndStep,
 }: UseFilterArrowsParams) => {
   return useMemo(() => {
-    return (arrow: Arrow) => {
-      const geometry = getArrowGeometry(arrow)
-      const { shaftStart, shaftEnd, heads } = geometry
-
-      if (
-        isPointOnScreen(shaftStart) ||
-        isPointOnScreen(shaftEnd) ||
-        heads.some(
-          (head) =>
-            isPointOnScreen(head.tip) ||
-            isPointOnScreen(head.leftWing) ||
-            isPointOnScreen(head.rightWing) ||
-            isPointOnScreen(head.base),
-        )
-      ) {
-        return true
-      }
-
-      const segments: Array<[Point, Point]> = [
-        [shaftStart, shaftEnd],
-        ...heads.flatMap((head) => [
-          [head.base, head.leftWing] as [Point, Point],
-          [head.leftWing, head.tip] as [Point, Point],
-          [head.tip, head.rightWing] as [Point, Point],
-          [head.rightWing, head.base] as [Point, Point],
-        ]),
-      ]
-
-      return segments.some(([p1, p2]) => doesLineIntersectViewport(p1, p2))
-    }
-  }, [isPointOnScreen, doesLineIntersectViewport])
+    return (arrow: Arrow) =>
+      isArrowVisible(arrow, {
+        isPointOnScreen,
+        doesLineIntersectViewport,
+        filterLayerAndStep,
+      })
+  }, [isPointOnScreen, doesLineIntersectViewport, filterLayerAndStep])
 }
